d070b56fbd72d3153c49c1a625ec22b23dcc633a
Innokentii Kozlov <bopuclav42@gmail.com>
2023-06-06 17:36:48 +0300

    Initial commit.

---
+++ python_logger/Logger.py
@@ -0,0 +1,44 @@
+import os
+import logging
+import datetime
+from Formatter import CustomFormatter as Formatter
+
+
+class Logger:
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    fmt = '%(asctime)s | %(message)s'
+
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.setFormatter(Formatter(fmt))
+
+    today = datetime.date.today()
+    file_handler = logging.FileHandler(os.getenv('FILE_HANDLER_PATH').format(today.strftime('%Y_%m_%d')))
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(fmt))
+
+    logger.addHandler(stdout_handler)
+    logger.addHandler(file_handler)
+
+    @staticmethod
+    def debug(message):
+        Logger.logger.debug(message)
+
+    @staticmethod
+    def info(message):
+        Logger.logger.info(message)
+
+    @staticmethod
+    def warning(message):
+        Logger.logger.warning(message)
+
+    @staticmethod
+    def error(message):
+        Logger.logger.error(message)
+
+    @staticmethod
+    def critical(message):
+        Logger.logger.critical(message)
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python_logger',
+    version='1.0.0',
+    author='Innokentii Kozlov',
+    author_email='innokentiikozlov@gmail.com',
+    description='Python Logger',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Hiava-Oy/python-logger',
+    license='MIT',
+    packages=['python_logger'],
+)
