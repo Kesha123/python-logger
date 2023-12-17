bde330a3664049d6e2ba2a9df076ba279e8edd1e
Innokentii Kozlov <innokentiikozlov@gmail.com>
2023-12-17 14:17:22 +0200

    warning coloring. coloring argument

--- python_logger/Formatter.py
+++ python_logger/Formatter.py
@@ -3,15 +3,47 @@ from python_logger.Colors import Colors
 
 class Formatter(logging.Formatter):
 
-    def __init__(self, fmt):
+    def __init__(
+            self,
+            fmt,
+            colored_message = True,
+            warning_yellow = False
+        ):
         super().__init__()
         self.fmt = fmt
         self.FORMATS = {
-            logging.DEBUG: Colors.GREEN_BACKGROUND + "DEBUG" + Colors.END + " " + Colors.green + self.fmt + Colors.END,
-            logging.INFO: Colors.CYAN_BACKGROUND  + "INFO" + Colors.END + " " + Colors.blue + self.fmt + Colors.END,
-            logging.WARNING: Colors.BROWN_BACKGROUND  + "WARNING" + Colors.END + " " + Colors.YELLOW + self.fmt + Colors.END,
-            logging.ERROR: Colors.RED_BACKGROUND  + "ERROR" + Colors.END + " " + Colors.red + self.fmt + Colors.END,
-            logging.CRITICAL: Colors.RED_BACKGROUND_BLINK + "CRITICAL" + Colors.END + " " + self.fmt,
+            logging.DEBUG:
+                Colors.GREEN_BACKGROUND + "DEBUG" + Colors.END + " " +
+                (
+                    Colors.green + self.fmt + Colors.END if colored_message
+                    else self.fmt
+                ),
+
+            logging.INFO:
+                Colors.CYAN_BACKGROUND  + "INFO" + Colors.END + " " +
+                (
+                    Colors.blue + self.fmt + Colors.END if colored_message
+                    else self.fmt
+                ),
+
+            logging.WARNING:
+                (Colors.YELLOW_BACKGROUND if warning_yellow else Colors.BROWN_BACKGROUND) +
+                "WARNING" + Colors.END + " " +
+                (
+                    Colors.YELLOW + self.fmt + Colors.END if colored_message
+                    else self.fmt
+                ),
+
+            logging.ERROR:
+                Colors.RED_BACKGROUND  + "ERROR" + Colors.END + " " +
+                (
+                    Colors.red + self.fmt + Colors.END if colored_message
+                    else self.fmt
+                ),
+
+            logging.CRITICAL:
+                Colors.RED_BACKGROUND_BLINK + "CRITICAL"
+                + Colors.END + " " + self.fmt,
         }
 
     def format(self, record):
--- python_logger/Logger.py
+++ python_logger/Logger.py
@@ -12,7 +12,11 @@ class Logger:
             cls._instance.__initialized = False
         return cls._instance
 
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            file: str = "./logs.log",
+            **kwargs
+        ):
         if not self.__initialized:
             self.logger = logging.getLogger(__name__)
             self.logger.setLevel(logging.DEBUG)
@@ -21,10 +25,11 @@ class Logger:
 
             stdout_handler = logging.StreamHandler()
             stdout_handler.setLevel(logging.DEBUG)
-            stdout_handler.setFormatter(Formatter(fmt))
+            formatter = Formatter(fmt, **kwargs)
+            stdout_handler.setFormatter(formatter)
 
             today = datetime.date.today()
-            file_handler = logging.FileHandler((kwargs.get("file") if kwargs.get("file") else "./logs.log").format(today.strftime('%Y_%m_%d')))
+            file_handler = logging.FileHandler((file).format(today.strftime('%Y_%m_%d')))
             file_handler.setLevel(logging.DEBUG)
             file_handler.setFormatter(logging.Formatter(fmt))
 
